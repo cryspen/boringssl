ddd33bfdf0a1a658fb3af6faca227ddf992ab5bc
Franziskus Kiefer <franziskuskiefer@gmail.com>
2025-02-20 15:20:20 +0100

    libcrux ml-kem 768
    This change adds the verified libcrux ml-kem code into the third_party
    directory. There are two versions, portable and avx2.
    An optimized, but unverified, impelmentation of SHA-3 is provided in
    there as well. ML-KEM performance heavily depends on SHA-3 and a 4x API
    is used in some cases, which is not generally useful.
    
    Libcrux ML-LEM is only used without OPENSSL_SMALL to control code size.
    
    A more detailed description on how to regenerate and verify the code can be found in https://github.com/cryspen/libcrux/blob/main/libcrux-ml-kem/TOOLS.md
    
    Change-Id: Iaf9d5729d22fae4a3630b0dade6b4d795eb03733

--- gen/sources.mk
+++ gen/sources.mk
@@ -80,6 +80,7 @@ boringssl_bcm_internal_headers := \
   crypto/fipsmodule/mldsa/fips_known_values.inc \
   crypto/fipsmodule/mldsa/mldsa.cc.inc \
   crypto/fipsmodule/mlkem/fips_known_values.inc \
+  crypto/fipsmodule/mlkem/libcrux_mlkem.cc.inc \
   crypto/fipsmodule/mlkem/mlkem.cc.inc \
   crypto/fipsmodule/rand/ctrdrbg.cc.inc \
   crypto/fipsmodule/rand/rand.cc.inc \
@@ -652,7 +653,16 @@ boringssl_crypto_internal_headers := \
   third_party/fiat/curve25519_64_msvc.h \
   third_party/fiat/p256_32.h \
   third_party/fiat/p256_64.h \
-  third_party/fiat/p256_64_msvc.h
+  third_party/fiat/p256_64_msvc.h \
+  third_party/libcrux/eurydice_glue.h \
+  third_party/libcrux/intrinsics/libcrux_intrinsics_avx2.h \
+  third_party/libcrux/karamel/target.h \
+  third_party/libcrux/libcrux_ct_ops.h \
+  third_party/libcrux/libcrux_mlkem768_avx2.h \
+  third_party/libcrux/libcrux_mlkem768_portable.h \
+  third_party/libcrux/libcrux_mlkem_core.h \
+  third_party/libcrux/libcrux_sha3_avx2.h \
+  third_party/libcrux/libcrux_sha3_portable.h
 
 boringssl_crypto_sources_asm := \
   crypto/curve25519/asm/x25519-asm-arm.S \
